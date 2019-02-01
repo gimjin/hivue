954e55c819eca7272ae547b744d7e2ba1f607f6a
Kim(夏尔猫叔) <kimseongrim@gmail.com>
2019-02-01 16:12:33 +0800

    Update .babelrc.js

--- .babelrc.js
+++ .babelrc.js
@@ -1,7 +1,4 @@
 module.exports = {
   presets: ['@babel/preset-env'],
-  plugins: [
-    '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-proposal-function-bind'
-  ]
+  plugins: ['@babel/plugin-syntax-dynamic-import']
 }
