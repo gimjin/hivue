eeb14ff33d2fddd1c2cecccff2e128ed7e3c7eaa
Kim(夏尔猫叔) <kimseongrim@gmail.com>
2019-05-27 17:36:18 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,14 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/max-attributes-per-line': ['error', {
+      'singleline': 3,
+      'multiline': {
+        'max': 1,
+        'allowFirstLine': false
+      }
+    }]
   },
   env: {
     browser: true,
