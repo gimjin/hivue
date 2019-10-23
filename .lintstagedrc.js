1751dd9c7d772756b198d7d449c917da6f7203dc
Kim(夏尔猫叔) <kimseongrim@gmail.com>
2019-10-23 21:14:06 +0800

    Update .lintstagedrc.js

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.{js,vue}': ['eslint --fix', 'git add'],
+  '*.{js,vue}': ['cross-env NODE_ENV=production eslint --fix', 'git add'],
   '*.{vue,htm,html,css,sss,less,scss,sass}': ['stylelint --fix', 'git add'],
   '*.{png,jpeg,jpg,gif,svg}': ['imagemin-lint-staged', 'git add']
 }
