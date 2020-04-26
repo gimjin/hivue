901bc51dde1a36d92d330d20a321a0b0b1eb0181
kim <kimseongrim@qq.com>
2020-04-26 19:05:38 +0800

    feat: router use router.app get prototype property

--- src/router/index.js
+++ src/router/index.js
@@ -3,17 +3,24 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-export default new VueRouter({
+const router = new VueRouter({
   /* eslint-disable */
   mode: ROUTER_MODE,
   base: ROUTER_BASE,
   /* eslint-enable */
   routes: [{
     path: '/',
-    component: () => import('@/components/index/index.vue'),
+    component: () => import('@/components/Parent.vue'),
     children: [{
       path: 'child',
       component: () => import('@/components/Child.vue')
     }]
   }]
 })
+
+router.beforeEach((to, from, next) => {
+  console.info('SSR:', router.app.$isServer) // this.$isServer in router
+  next()
+})
+
+export default router
