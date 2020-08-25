03d2036ee77c7417d7cab39de45014d930f204b0
Kim(夏尔猫叔) <kimseongrim@icloud.com>
2020-08-25 09:25:07 +0800

    Update .eslintrc.js
    add no-console and no-debugger control.

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,8 @@ module.exports = {
     'jsdoc'
   ],
   rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'vue/max-attributes-per-line': ['error', {
       'singleline': 3,
       'multiline': {
